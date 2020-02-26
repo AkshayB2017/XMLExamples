141432957a302bd712b342d9a383ad33be01d5cc
AkshayB2017 <akbgunner4ever@gmail.com>
2020-02-26 14:03:02 +0530

    Changes made to generalmapper.py

--- mappergeneral.py
+++ mappergeneral.py
@@ -1,23 +1,60 @@
 import xml.etree.ElementTree as ET
-
-
+def mapper(j):
+    for i in range(j,len(tag)-1):
+        if(tag[i]=='expression'):
+            expression(i)
+        if(tag[i]=='assignment'):
+            assigment(i)
+        if(tag[j]=='if'):
+            if_expression(i)
+        if(tag[i]=='else'):
+            else_expression(i)
+        if(tag[i]=='print'):
+            print_expression(i)
+        if(tag[i]=='input'):
+            input_expression(i)
+        if(tag[i]=='main'):
+            main_func(i)
+    f.write("return 0;\n")
+    f.write("}")
+    
+def main_func(i):
+    f.write("int main()\n{\n")
 
 def expression(i):
-    if(tag[i+1]=='constant') or (tag[i+1]=='variable'):
+    if(tag[i+1]=='constant'):
         f.write(text[i+1])
+        i+=1
+    if(tag[i+1]=='variable'):
+        f.write(text[i+2])
+        i+=2
     if(tag[i+1]=='function_call'):
         f.write(text[i+1])
     f.write(";")
-    i+=2
+def variable(i):
+    if(tag[i+1]=='var_name'):
+        f.write(text[i+1])
+        i+=1
+        f.write(';\n')
+    if(tag[i+1]=='var_type'):
+        f.write(text[i+1])
+        f.write('')
+        f.write(text[i+2])
+        f.write(';\n')
+        i+=2
+def operator(i):
+    f.write(text[i+1])
+    i+=1
+
 
 def assignment(i):
     if(tag[i+1]=='variable'):
         f.write(text[i+2])
         f.write('')
         f.write(text[i+3])
-        f.write('= ')
+        f.write('=')
         if(tag[i+4]=='expression'):
-            expression()
+            expression(i)
     f.write(";")
 
 def if_expression(i):
@@ -33,7 +70,7 @@ def if_expression(i):
     
 def else_expression(i):
     f.write('else {\n')
-    body()
+    mapper(i)
     f.write('\n }')
 
 def for_loop(i):
@@ -50,7 +87,7 @@ def for_loop(i):
         if(tag[i+1]=='assignment'):
             assignment()
     f.write(')\n{\n')
-    body()
+    mapper(i)
     f.write('\n}')
 
 def function(i):
@@ -67,31 +104,14 @@ def function(i):
 def print_expression(i):
     f.write("cout<<\"")
     f.write(text[i])
-    f.write("\"<<endl;")
+    f.write("\"<<endl;\n")
     i+=1
 
 def input_expression(i):
     f.write("cin>>\"")
     f.write(text[i])
-    f.write("\">>endl;")  
+    f.write("\">>endl;\n")  
     i+=1
-
-
-def mapper(i):
-    while(i!=len(tag)):
-        if(tag[i]=='expression'):
-            expression(i)
-        if(tag[i]=='assignment'):
-            assigment(i)
-        if(tag[i]=='if'):
-            if_expression(i)
-        if(tag[i]=='else'):
-            else_expression(i)
-        if(tag[i]=='print'):
-            print_expression(i)
-        if(tag[i]=='input'):
-            input_expression(i)
-
 
 
 
@@ -113,4 +133,5 @@ for elem in myroot.iter():
     text.append(elem.text)
     attribute.append(elem.attrib)
 i=0
+f=open("final.txt","w")
 mapper(i)    
